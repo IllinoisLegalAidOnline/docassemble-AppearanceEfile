48b67e3a0cc65cb191bf6d6b0c16336a4b8350ca
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2022-06-09 08:44:43 -0400

    Bump version to 0.1.3a1 to test on dev server

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.Appearance',
-      version='0.1.2.7',
+      version='0.1.3a1',
       description=('Appearance'),
       long_description='# docassemble.Appearance\n\nAppearance\n\n## Author\n\nMatt Newsted, mnewsted@illinoislegalaid.org\n\n',
       long_description_content_type='text/markdown',
